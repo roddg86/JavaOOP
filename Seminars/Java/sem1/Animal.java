6395dd1f4c6c6c11a62111cd370333a99f118dc3
Dmitry <110647769+roddg86@users.noreply.github.com>
2023-03-03 11:56:09 +0900

    add constr

---
+++ Seminars/Java/sem1/Animal.java
@@ -0,0 +1,16 @@
+package Seminars.Java.sem1;
+
+public class Animal {
+    // поля класса
+    public String name;
+    private String color;
+    private int pawsCount;
+
+    // конструктор класса
+    public Animal(String name, String color, int pawsCount) {
+        this.name = name;
+        this.color = color;
+        this.pawsCount = pawsCount;
+    }
+
+}
