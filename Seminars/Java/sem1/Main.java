037059f9b845428404ba7493c38e9f391fcc9d65
Dmitry <110647769+roddg86@users.noreply.github.com>
2023-03-03 14:08:42 +0900

    info constructor

--- Seminars/Java/sem1/Main.java
+++ Seminars/Java/sem1/Main.java
@@ -3,6 +3,10 @@ package Seminars.Java.sem1;
 public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal(null,null,1);
+        System.out.println(animal.getColor());
+
+        Animal animal1 = new Animal("собака");
+        System.out.println(animal1.getPawsCount());
         
     }
     
